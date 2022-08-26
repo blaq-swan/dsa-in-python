84b0d7cb027eb663d6ff4a4110c7e656b6ecb52d
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-27 00:51:57 +0300

    checks if a number is even using bitwise

--- python-primer/solutions/is_even.py
+++ python-primer/solutions/is_even.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""even or not"""
+
+
+def is_even(num) -> bool:
+    """checks if a number is even or not using bitwise
+    Args:
+        num: number to be checked
+    Returns: True if num is even, otherwise False
+    """
+
+    if not isinstance(num, int):
+        raise TypeError("num ")
+    return True if num & 1 == 0 else False
