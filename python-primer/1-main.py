851edc7d579d37ec969e334eeba3284b0bba6fc1
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-26 15:35:20 +0300

    source code test for is_multiple

---
+++ python-primer/1-main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from solutions.is_multiple import is_multiple
+
+if __name__ == "__main__":
+    try: 
+        n = 20
+        m = 5
+        print(is_multiple(n, m))
+
+        n = 3
+        m = 5
+        print(is_multiple(n, m))
+
+        n = 0
+        m = 5
+        print(is_multiple(n, m))
+
+        n = -5
+        m = 0
+        print(is_multiple(n, m))
+
+    except Exception as e:
+        print("Error!")
