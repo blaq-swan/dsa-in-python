64028c2fa255d02143a8e89c7dcb8205df071dd6
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-27 20:58:44 +0300

    main file for comp_sum_squares

---
+++ python-primer/5-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from solutions.comp_sum_squares import comp_square_sum
+
+
+if __name__ == "__main__":
+    print(comp_square_sum(10))
+    print(comp_square_sum(5))
+    print(comp_square_sum(500))
+    print(comp_square_sum(1024))
