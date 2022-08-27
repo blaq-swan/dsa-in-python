6c376e570757dcc63bb3788735990f9430008ba8
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-27 19:40:23 +0300

    sums the squares of all the positive integers smaller than n

--- python-primer/solutions/sum_squares.py
+++ python-primer/solutions/sum_squares.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+"""square_sum module"""
 
-def sum_square(n):
+
+def sum_square(n) -> int:
+    """
+    finds sum of the squares of all the positive integers smaller than n
+    Args:
+        n: number limit
+    Returns:
+        sum of all positive integers
+    """
+
     square_sum = []
     count = 0
     if not isinstance(n, int):
         raise TypeError("n must be an int")
-    if n <= 0:
+    if n < 0:
         raise ValueError("n must be positive")
     for i in range(n):
-        square_sum.append(i ** 2)
+        square_sum += [pow(i, 2)]
     for x in square_sum:
         count += x
     return count
-
-print(sum_square(-10))
