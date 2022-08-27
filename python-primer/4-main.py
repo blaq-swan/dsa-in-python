c3d758cdbe676a193f9b7e9a4ea4df19d24ffcc5
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-27 20:16:19 +0300

    main module for sum_squares

---
+++ python-primer/4-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from solutions.sum_squares import sum_squares
+
+
+if __name__ == "__main__":
+    print(sum_squares(10))
+    print(sum_squares(5))
+    print(sum_squares(500))
+    print(sum_squares(1024))
