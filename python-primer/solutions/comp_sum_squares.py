c244707d9e3078a6077e28583477906b5a462283
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-27 20:41:12 +0300

    sums all squares using comprehension

---
+++ python-primer/solutions/comp_sum_squares.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""comp_square_sum module"""
+
+
+def comp_square_sum(n) -> int:
+    """
+    finds sum of the squares of all the positive integers smaller than
+    Args:
+        n: number limit
+    Returns:
+        sum of all positive integers
+    """
+
+    assert int(n) == n and n > 0, "n must be a positive integer"
+
+    return sum([pow(x, 2) for x in range(n)])
