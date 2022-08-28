ccaa747bc504424703828fc7ee19f395a75edf8f
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-28 16:30:30 +0300

    main file for sum_squares_odd

---
+++ python-primer/6-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from solutions.sum_squares_odd import sum_odd_squares
+
+
+if __name__ == "__main__":
+    print(sum_odd_squares(32))
+    print(sum_odd_squares(64))
+    print(sum_odd_squares(512))
+    print(sum_odd_squares(1024))
