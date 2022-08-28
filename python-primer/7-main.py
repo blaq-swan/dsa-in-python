3d45863f50ef9e12b5ae8b70bc67529b43520458
Blaq Swan <mr.alangobryan@gmail.com>
2022-08-28 17:04:15 +0300

    main file for com_squares_odd

---
+++ python-primer/7-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+from solutions.comp_squares_odd import sum_odd_squares
+
+
+if __name__ == "__main__":
+    print(sum_odd_squares(10))
+    print(sum_odd_squares(32))
+    print(sum_odd_squares(64))
+    print(sum_odd_squares(128))
