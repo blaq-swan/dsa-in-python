7f0ed27859f9c47c24c027d4178954978ef0ac19
Blaq Swan <mr.alangobryan@gmail.com>
2022-09-01 07:14:54 -0700

    negative indexing proof

--- python-primer/solutions/negative_index.py
+++ python-primer/solutions/negative_index.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
-"""implementing python's negative indexing"""
+"""Proving the correlation of negative and psotive indexing"""
 
 
-def negative_index() -> list:
-    pass
+"""
+string = s
+s[k] = -n <= k < 0
+    k is less than 0
+        k is the index to be used
+
+    case:
+        length = 10
+            last element = index 9
+            To access last element:
+                a. s[9] (s[k])
+                b. s[-1] (s[-k])
+                    constants:
+        In other words:
+            s[len - 1] -> last element
+                s[len - 1] = s[-1]
+                Therefore:
+                    -n <= k < 0
+        In case where s[j], with j >= 0:
+            j >= 0 < n -> n >= j > 0
+
+    Ans:
+        n >= j > 0
+"""
