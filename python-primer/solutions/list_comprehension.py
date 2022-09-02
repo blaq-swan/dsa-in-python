3620ec24ac267369ffe4106e694a1428101f7db2
Blaq Swan <mr.alangobryan@gmail.com>
2022-09-02 00:20:41 -0700

    powers of base of 2

---
+++ python-primer/solutions/list_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""implementing list comprehension"""
+
+
+def powers_of_two():
+    """
+    exponents with a base of 2
+    Returns:
+        list with powe4rs of base 2
+    """
+
+    return [pow(2, x) for x in range(9)]
+
+
+print(powers_of_two())
